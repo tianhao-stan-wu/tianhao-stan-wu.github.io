bacd8d1259fa8b06648d62a42016c295887416e6
tianhao-stan-wu <97497708+tianhao-stan-wu@users.noreply.github.com>
2025-01-24 04:42:06 +0000

    Deploying to gh-pages from @ tianhao-stan-wu/tianhao-stan-wu.github.io@92eb2cf1dafb34b560eda0bd6f6e61551643ea47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -415,9 +415,9 @@ ninja.data = [{
           description: "USC CURVE fellowship",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-autonomous-drone",
+          title: 'autonomous drone',
+          description: "UCI summer research",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
